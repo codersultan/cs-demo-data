f9e60f83b8918475ebe703d05c48e7ef91090bad
Kalimi Sultan <developersultan100@gmail.com>
2024-09-07 01:27:56 +0600

    Demo Data for npm

---
+++ data.js
@@ -0,0 +1,636 @@
+// Studend demo data
+
+export const studentData = [
+  {
+    id: 1,
+    Name: "Kalimi Sultan",
+    FatherName: "Abdur Rashid",
+    MotherName: "Joynab Bibi",
+    DateofBirth: "12-04-2000",
+    isMale: true,
+    Institute: "ND Collage",
+    Roll: 1111,
+    Reg: 11111111,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Regular",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 1,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 35,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 85,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 89,
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    Name: "Abu Sayed Khan",
+    FatherName: "Abdus Samad",
+    MotherName: "Mst. Rabeya Bibi",
+    DateofBirth: "12-04-1999",
+    isMale: true,
+    Institute: "ND Collage",
+    Roll: 2222,
+    Reg: 22222222,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Irregular ",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 65,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 35,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 85,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 56,
+      },
+    ],
+  },
+  {
+    id: 3,
+    Name: "Arefin Rahman",
+    FatherName: "Md. Araf Rahman",
+    MotherName: "Mst. Afrin Rahman",
+    DateofBirth: "12-04-1998",
+    isMale: true,
+    Institute: "ND Collage",
+    Roll: 3333,
+    Reg: 33333333,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Regular",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 65,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 89,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 85,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 56,
+      },
+    ],
+  },
+  {
+    id: 4,
+    Name: "Farhan Talukdar",
+    FatherName: "Md. Fahim Talukdar",
+    MotherName: "Mst. Farjana Rahman",
+    DateofBirth: "12-04-2001",
+    isMale: true,
+    Institute: "ND Collage",
+    Roll: 4444,
+    Reg: 44444444,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Regular",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 65,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 35,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 85,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 56,
+      },
+    ],
+  },
+  {
+    id: 5,
+    Name: "Hossain Ahommed",
+    FatherName: "Md. Harun Ahommed",
+    MotherName: "Mst. Hazra Ahommed",
+    DateofBirth: "12-04-1999",
+    isMale: true,
+    Institute: "ND Collage",
+    Roll: 5555,
+    Reg: 55555555,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Regular",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 65,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 35,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 85,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 56,
+      },
+    ],
+  },
+  {
+    id: 6,
+    Name: "Marufa Akhter",
+    FatherName: "Md. Morshed Alom",
+    MotherName: "Mst. Majeda Bibi",
+    DateofBirth: "12-04-1998",
+    isMale: false,
+    Institute: "ND Collage",
+    Roll: 6666,
+    Reg: 66666666,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Irregular",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 65,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 35,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 85,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 56,
+      },
+    ],
+  },
+  {
+    id: 7,
+    Name: "Mim Akhter",
+    FatherName: "Md. Mizan Sorkar",
+    MotherName: "Mst. Monjara Bibi",
+    DateofBirth: "12-04-2001",
+    isMale: false,
+    Institute: "ND Collage",
+    Roll: 7777,
+    Reg: 77777777,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Regular",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 65,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 35,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 85,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 56,
+      },
+    ],
+  },
+  {
+    id: 8,
+    Name: "Mamun Mirdha",
+    FatherName: "Md. Barik Hosen",
+    MotherName: "Mst. Rukhsana Begum",
+    DateofBirth: "12-04-2000",
+    isMale: true,
+    Institute: "ND Collage",
+    Roll: 8888,
+    Reg: 88888888,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Regular",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 65,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 35,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 85,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 56,
+      },
+    ],
+  },
+  {
+    id: 9,
+    Name: "Kawsar Ali",
+    FatherName: "Md. Kamruzzaman",
+    MotherName: "Mst. Angura Bibi",
+    DatofBirth: "12-04-1997",
+    isMale: true,
+    Institute: "ND Collage",
+    Roll: 9999,
+    Reg: 99999999,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Irregular",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 65,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 35,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 85,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 56,
+      },
+    ],
+  },
+  {
+    id: 10,
+    Name: "Jesmin Akhter",
+    FatherName: "Abu Bokkor Siddik",
+    MotherName: "Mst. Angura Bibi",
+    DateofBirth: "28-11-2001",
+    isMale: false,
+    Institute: "ND Collage",
+    Roll: 1212,
+    Reg: 12121212,
+    Board: "Rajshahi",
+    Group: "Science",
+    Type: "Regular",
+
+    Result: [
+      {
+        SubjectName: "Bangla",
+        Code: "101",
+        Mark: 65,
+      },
+      {
+        SubjectName: "English",
+        Code: "102",
+        Mark: 35,
+      },
+      {
+        SubjectName: "Physics",
+        Code: "103",
+        Mark: 10,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "104",
+        Mark: 54,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "105",
+        Mark: 67,
+      },
+      {
+        SubjectName: "Bangla",
+        Code: "106",
+        Mark: 56,
+      },
+    ],
+  },
+];
+
+// Team Membars Demo Data
+export const teamMembers = [
+  {
+    id: 1,
+    name: "Ashraful Haq",
+    skill: "CEO",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-01.png",
+    facebook: "https://www.facebook.com/kalimisultan/",
+    twitter: "https://x.com/kalimi_sultan",
+    linkdin: "https://www.facebook.com/kalimisultan/",
+    instagram: "https://www.instagram.com/codersultan/",
+    youtube: "https://www.youtube.com/@codersultan",
+  },
+  {
+    id: 2,
+    name: "Kalimi Sultan",
+    skill: "MERN Stack Devs",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-13.png",
+    facebook: "https://www.facebook.com/kalimisultan/",
+    twitter: "https://x.com/kalimi_sultan",
+    linkdin: "https://www.facebook.com/kalimisultan/",
+    instagram: "https://www.instagram.com/codersultan/",
+    youtube: "https://www.youtube.com/@codersultan",
+  },
+  {
+    id: 3,
+    name: "Kawser Ali",
+    skill: "IOS Devs",
+    photo: "https://unitedthemes.com/wp-content/uploads/2018/09/team2.jpg",
+    facebook: "https://www.facebook.com/kalimisultan/",
+    twitter: "https://x.com/kalimi_sultan",
+    linkdin: "https://www.facebook.com/kalimisultan/",
+    instagram: "https://www.instagram.com/codersultan/",
+    youtube: "https://www.youtube.com/@codersultan",
+  },
+  {
+    id: 4,
+    name: "Abu Sayed",
+    skill: "Android Devs",
+    photo: "https://unitedthemes.com/wp-content/uploads/2018/09/team1.jpg",
+    facebook: "https://www.facebook.com/kalimisultan/",
+    twitter: "https://x.com/kalimi_sultan",
+    linkdin: "https://www.facebook.com/kalimisultan/",
+    instagram: "https://www.instagram.com/codersultan/",
+    youtube: "https://www.youtube.com/@codersultan",
+  },
+  {
+    id: 5,
+    name: "Abu Sayed",
+    skill: "Android Devs",
+    photo: "https://unitedthemes.com/wp-content/uploads/2018/09/team1.jpg",
+    facebook: "https://www.facebook.com/kalimisultan/",
+    twitter: "https://x.com/kalimi_sultan",
+    linkdin: "https://www.facebook.com/kalimisultan/",
+    instagram: "https://www.instagram.com/codersultan/",
+    youtube: "https://www.youtube.com/@codersultan",
+  },
+  {
+    id: 6,
+    name: "Kalimi Sultan",
+    skill: "MERN Stack Devs",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-13.png",
+    facebook: "https://www.facebook.com/kalimisultan/",
+    twitter: "https://x.com/kalimi_sultan",
+    linkdin: "https://www.facebook.com/kalimisultan/",
+    instagram: "https://www.instagram.com/codersultan/",
+    youtube: "https://www.youtube.com/@codersultan",
+  },
+  {
+    id: 7,
+    name: "Kawser Ali",
+    skill: "IOS Devs",
+    photo: "https://unitedthemes.com/wp-content/uploads/2018/09/team2.jpg",
+    facebook: "https://www.facebook.com/kalimisultan/",
+    twitter: "https://x.com/kalimi_sultan",
+    linkdin: "https://www.facebook.com/kalimisultan/",
+    instagram: "https://www.instagram.com/codersultan/",
+    youtube: "https://www.youtube.com/@codersultan",
+  },
+  {
+    id: 8,
+    name: "Ashraful Haq",
+    skill: "Product Designer",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-01.png",
+    facebook: "https://www.facebook.com/kalimisultan/",
+    twitter: "https://x.com/kalimi_sultan",
+    linkdin: "https://www.facebook.com/kalimisultan/",
+    instagram: "https://www.instagram.com/codersultan/",
+    youtube: "https://www.youtube.com/@codersultan",
+  },
+];
+
+// Demo Post Data
+
+export const demoPost = [
+  {
+    id: "1",
+    createdAt: "",
+    featuredImg:
+      "https://tds-images.thedailystar.net/sites/default/files/styles/big_202/public/images/2024/09/06/babe.jpg",
+    anthor: "The Daily Star",
+    title: "Dewanbag Sharif attacked, torched in Nganj",
+    description:
+      "Miscreants attacked and torched the Babe Jannat Dewanbag Sharif, a shrine in Madanpur area of Narayanganj's Bandar upazila, this morning. At least four persons injured in the incident are undergoing treatment at Dhaka Medical College Hospital, said police. Google News LinkFor all latest news, follow The Daily Star's Google News channel. According to locals and police sources, some people broke into the shrine around 6:00am and carried out arson and vandalism.",
+  },
+  {
+    id: "2",
+    createdAt: "",
+    featuredImg:
+      "https://tds-images.thedailystar.net/sites/default/files/styles/big_202/public/media/api_images/2024/09/06/Gaza%20update.jpg",
+    anthor: "The Daily Star",
+    title: "Israel 'winding down' West Bank offensive",
+    description:
+      "The reported pull-out came with Israel at loggerheads with its main ally the United States over talks aimed at forging a truce in the Gaza war, now nearly in its 12th month. On Thursday, US Secretary of State Antony Blinken urged both Israel and Hamas to finalise a truce deal, saying: I think based on what I've seen, 90 percent is agreed.",
+  },
+  {
+    id: "3",
+    createdAt: "",
+    featuredImg:
+      "https://tds-images.thedailystar.net/sites/default/files/styles/big_202/public/images/2024/09/06/babe.jpg",
+    anthor: "The Daily Star",
+    title: "Ebadot may go with team to India",
+    description:
+      "Ebadot suffered an anterior cruciate ligament injury last year during the Afghanistan series and had to undergo surgery in London in August which put him out of action for two World Cup events. Last month Ebadot said that he was hopeful of making the India Tests. On Friday, he said he may be carried with the side to monitor his progress. It takes a long time to recover from the kind of injury that I had. It's a difficult time. Don't think it will take too long to recover now. I will play and try to do well, Ebadot told reporters at Mirpur's Sher-e-Bangla National Cricket Stadium on Friday.",
+  },
+  {
+    id: "4",
+    createdAt: "",
+    featuredImg:
+      "https://tds-images.thedailystar.net/sites/default/files/styles/big_202/public/images/2024/09/05/brendon_mccullum_.jpg",
+    anthor: "The Daily Star",
+    title: "'I'm happy to be governed by results'",
+    description:
+      "The New Zealander has reinvigorated England's Test set-up alongside captain Ben Stokes since 2022 and will seek to bring his attacking Bazball approach to the limited-overs sides. Google News LinkFor all latest news, follow The Daily Star's Google News channel. McCullum, 42, has extended his contract until the end of 2027, officially taking over the one-day international and Twenty20 sides in January to coincide with a tour of India and the Champions Trophy in Pakistan. He will succeed Matthew Mott, sacked as coach of England's white-ball teams in July following lacklustre defences of their 50-over and Twenty20 World Cup titles.",
+  },
+  {
+    id: "5",
+    createdAt: "",
+    featuredImg:
+      "https://tds-images.thedailystar.net/sites/default/files/styles/big_202/public/images/2024/09/04/afp_20210423_98k94z_v2_highres_cricketsriban.jpg",
+    anthor: "The Daily Star",
+    title: "Test continuity in demand now",
+    description:
+      "Bangladesh's Test win at Mount Maunganui against the then World Test champions New Zealand in 2022 would certainly go down as a historic achievement in the Tigers' history. But even Mominul Haque, the Tigers' skipper back then, believes that winning a series away from home evokes a very different kind of feeling within the team than what a one-off win produces. The Mount Maunganui win was clinched under hostile conditions against one of the best Test sides in the world. But the following Test in Christchurch did not follow a similar script.",
+  },
+  {
+    id: "6",
+    createdAt: "",
+    featuredImg:
+      "https://tds-images.thedailystar.net/sites/default/files/styles/big_202/public/images/2024/09/06/babe.jpg",
+    anthor: "The Daily Star",
+    title: "US job openings hit 3-1/2-year low as labor market eases",
+    description:
+      "The larger-than-expected decline in unfilled jobs shown in the Job Openings and Labor Turnover Survey, or JOLTS report, from the Labor Department on Wednesday meant there were 1.07 open positions for every unemployed person in July. That was the least since May 2021 and down from 1.16 in June. The vacancies-to-unemployed ratio peaked just above 2.0 in 2022. Still, the labor market is likely not deteriorating. A separate report from the Fed described employment levels as generally flat to up slightly in recent weeks.",
+  },
+];
